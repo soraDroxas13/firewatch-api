fb8c50a2f0244f503d638565a2e224f3bdc7466a
soraDroxas13 <soradroxas13@gmail.com>
2026-05-22 04:24:55 +0100

    fix: configuration CORS production

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const cors    = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://projet-fire-watch.vercel.app',
+    'http://localhost:4200'
+  ],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
